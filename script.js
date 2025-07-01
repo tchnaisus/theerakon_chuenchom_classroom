1ef833fbf611e8969074863d64bf6198b68563e2
Theerakon Chuenchom <151618502+tchnaisus@users.noreply.github.com>
2025-06-30 23:53:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function () {
   document.body.classList.add('loaded');
 });
+ document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
